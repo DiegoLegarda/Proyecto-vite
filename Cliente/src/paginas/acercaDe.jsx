90919367b6ff2ad4e3d49fd9cb4b90be752961c1
dlegarda <legarda8027@gmail.com>
2024-09-30 06:40:26 -0500

    Modulo de estilo

--- Cliente/src/paginas/acercaDe.jsx
+++ Cliente/src/paginas/acercaDe.jsx
@@ -1,11 +1,12 @@
 import BarraNav from  "../componentes/barraNav.jsx";
 import { Link } from "react-router-dom";
+import styles from '../estilos/acerca.module.css'
 const acerca=()=>{
     return (
-        <div className="container">            
-            <div className="row">
+        <div className={styles.container}>            
+            <div className={styles.row}>
                 <div className="col-md-12"> 
-                    <h1 className="text-center">Acerca de Nosotros</h1>
+                    <h1 className={styles.textCenter}>Acerca de Nosotros</h1>
                     <Link to='/Herramientas'>Herramientas</Link> 
                 </div>
             </div>
