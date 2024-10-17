43ac788b69c96c2805f557df4607bb58833d6555
dlegarda <legarda8027@gmail.com>
2024-10-16 22:47:05 -0500

    comunicacion con el servidor

--- Cliente/src/componentes/manejoFormulario.jsx
+++ Cliente/src/componentes/manejoFormulario.jsx
@@ -1,16 +1,85 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 function ManejoFormulario() {
-    const [name, setName] = useState("");
+    const [datosForm, setDatosForm] = useState({ nombre: '', correo: '', contraseña: '' });
+    const [datos, setDatos] = useState({});
+    const [enviando, setEnviando] = useState(false);
+    const [error, setError] = useState(null);
+    const [registroExitoso, setRegistroExitoso] = useState(false);
+
+    const manejoCambios = (e) => {
+        const { name, value } = e.target;
+        setDatosForm({ ...datosForm, [name]: value });
+    };
+    const URL = 'http://localhost:3000/api/usuariosDB';
+    const manejoSumision = (e) => {
+        e.preventDefault();
+        if (enviando) return;
+        setEnviando(true);
+        console.log(datosForm);
+        axios.post(URL, datosForm)
+            .then((response) => {
+                setDatos(response.data);
+                console.log(datos);
+                setRegistroExitoso(true);
+            })
+            .catch((error) => {
+                if (error.response && error.response.data) {
+                    setError(`Error: ${error.response.data.mensaje}`); 
+                } else {
+                    setError(`Error: ${error.message}`); 
+                }
+                console.log('Error al enviar el formulario:',error);
+            })
+            .finally(() => {
+                setEnviando(false);
+            });
+    }
 
     return (
-        <form>
-            <label>Introduzca su nombre:
-                <input type="text"
-                    value={name}
-                    onChange={(evento) => setName(evento.target.value)}
+        <form onSubmit={manejoSumision}>
+            <label>
+                Ingrese nombre:
+                <input
+                    type="text"
+                    name="nombre"
+                    value={datosForm.nombre || ""}
+                    onChange={manejoCambios}
                 />
-                <p>{name}</p>
             </label>
+            <br />
+            <label>
+                Ingrese correo:
+                <input
+                    type="mail"
+                    name="correo"
+                    value={datosForm.correo || ""}
+                    onChange={manejoCambios}
+                />
+            </label>
+            <label>
+                Ingrese contraseña:
+                <input
+                    type="password"
+                    name="contraseña"
+                    value={datosForm.contraseña || ""}
+                    onChange={manejoCambios}
+                />
+            </label>
+            <br />
+            <button type="submit" disabled={enviando}>
+                {enviando ? 'Enviando...' : 'Enviar'}
+            </button>
+            {registroExitoso && (
+                <div>
+                    <p>Usuario registrado exitosamente:</p>
+                    <p>Nombre: {datos.nombre}</p>
+                    <p>Correo: {datos.correo}</p>
+                    <p>Fecha de Registro: {new Date(datos.fechaRegistro).toLocaleString()}</p>
+                </div>
+            )}
+            {error && <p style={{ color: 'red' }}>{error}</p>}
         </form>
     )
 }
--- Cliente/src/paginas/registro.jsx
+++ Cliente/src/paginas/registro.jsx
@@ -1,10 +1,12 @@
 import ManejoFormulari2 from '../componentes/manejoFormulario2'
+import ManejoFormulario from '../componentes/manejoFormulario'
 import BarraNav from  "../componentes/barraNav.jsx";
+import ManejoFormulario2 from '../componentes/manejoFormulario2';
 function Registro(){
     return(
         <div>        
             <h1>Registro</h1>
-            <ManejoFormulari2/>
+            <ManejoFormulario/>
         </div>
     );
 }
